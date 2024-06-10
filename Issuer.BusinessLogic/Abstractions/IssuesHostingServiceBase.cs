37356e1e6101d507a34d133d68626c092574b8e7
mgrzegor <m-grzegorczyk@o2.pl>
2024-06-10 14:15:12 +0200

    git services refactor - abstraction base

---
+++ Issuer.BusinessLogic/Abstractions/IssuesHostingServiceBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Issuer.BusinessLogic.Abstractions
+{
+    public abstract class IssuesHostingServiceBase
+    {
+        protected readonly HttpClient _httpClient;
+        protected readonly string _authToken;
+
+        protected IssuesHostingServiceBase(string authToken)
+        {
+            _httpClient = new HttpClient();
+            _authToken = authToken;
+        }
+
+        protected HttpRequestMessage CreateRequest(HttpMethod method, string url, object data = null)
+        {
+            var request = new HttpRequestMessage(method, url);
+
+            ConfigureRequestHeaders(request);
+
+            if (data != null)
+            {
+                var content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
+                request.Content = content;
+            }
+
+            return request;
+        }
+
+        protected async Task<string> SendRequestAsync(HttpRequestMessage request)
+        {
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        protected abstract void ConfigureRequestHeaders(HttpRequestMessage request);
+    }
+}
